d510c02ee8a090100a164ca6cbe6f53c46c53e7b
Ivan Habunek <ivan@habunek.com>
2022-03-08 10:23:42 +0100

    Set up packaging

--- mix.exs
+++ mix.exs
@@ -2,7 +2,7 @@ defmodule LibPhoneNumber.MixProject do
   use Mix.Project
 
   @version "0.1.0"
-  @scm_url "https://github.com/ihabunek/ex_libphonenumber_js/"
+  @scm_url "https://github.com/ihabunek/ex_libphonenumber_js"
 
   def project do
     [
@@ -11,8 +11,9 @@ defmodule LibPhoneNumber.MixProject do
       elixir: "~> 1.13",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
-      preferred_cli_env: [docs: :docs],
       docs: docs(),
+      package: package(),
+      preferred_cli_env: [docs: :docs],
       source_url: @scm_url,
       description: "Provides parsing and formatting phone numbers."
     ]
@@ -36,6 +37,18 @@ defmodule LibPhoneNumber.MixProject do
       source_ref: @version,
       main: "readme",
       extras: ["README.md", "CHANGELOG.md", "LICENSE"]
+    ]
+  end
+
+  defp package() do
+    [
+      description: "Parsing and formatting phone numbers.",
+      maintainers: ["Ivan Habunek"],
+      licenses: ["Apache-2.0"],
+      links: %{
+        "Github" => @scm_url,
+        "Changelog" => "#{@scm_url}/blob/master/CHANGELOG.md"
+      }
     ]
   end
 end
